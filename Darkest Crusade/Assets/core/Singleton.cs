d8ade040581d747ea4b92c77752615583000da10
Dawid Yerginyan <dawid.yerginyan@gmail.com>
2018-11-25 23:38:45 +0100

    feat(Core): added singleton, gamestate, MonoBehaviour extensions - Singleton generic class - GameState persistance object - Component and GameObject extensions to GetComponent() and GetOrAddComponent() - Added empty game loader - Modified HeroesFactory to use GameState - Added empty EnemyManager

---
+++ Darkest Crusade/Assets/core/Singleton.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using Core.Extensions;
+
+namespace Core
+{
+
+  /// <summary>
+  /// Inherit from this base class to create a singleton.
+  /// e.g. public class MyClassName : Singleton<MyClassName> {}
+  /// </summary>
+  public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+  {
+    private static bool _shuttingDown = false;
+    private static object _lock = new object();
+    private static T _instance;
+
+    /// <summary>
+    /// Access singleton instance through this propriety.
+    /// </summary>
+    public static T Instance
+    {
+      get
+      {
+        if (_shuttingDown)
+        {
+          Debug.LogWarning(
+            "[Singleton] Instance '"
+            + typeof(T) +
+            "' already destroyed. Returning null."
+          );
+
+          return null;
+        }
+
+        lock (_lock)
+        {
+          if (_instance == null)
+          {
+            _instance = (T)FindObjectOfType(typeof(T));
+            if (_instance == null)
+            {
+              var singletonObject = new GameObject();
+              _instance = singletonObject.GetOrAddComponent<T>();
+              singletonObject.name = typeof(T).ToString() + " (Singleton)";
+
+              DontDestroyOnLoad(singletonObject);
+            }
+          }
+
+          return _instance;
+        }
+      }
+    }
+
+    private void OnApplicationQuit()
+    {
+      _shuttingDown = true;
+    }
+
+    private void OnDestroy()
+    {
+      _shuttingDown = true;
+    }
+  }
+
+}
