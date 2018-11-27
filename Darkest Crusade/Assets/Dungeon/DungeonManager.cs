205ae2e945021e462b29b58f74e2bec310220304
Dawid Yerginyan <dawid.yerginyan@gmail.com>
2018-11-27 01:23:22 +0100

    feat(Level generator): bare essentials

---
+++ Darkest Crusade/Assets/Dungeon/DungeonManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonManager : MonoBehaviour {
+  public int[] dungeonBounds = { 0, 450 };
+  public int minDungeonlength = 50;
+  public int maxDungeonLength = 300;
+  public Material _material;
+
+
+  
+  private static Random rng = new Random();
+
+  void Awake()
+  {
+
+  }
+
+  void Start()
+  {
+    foreach (SpriteRenderer item in GameObject.FindObjectsOfType(typeof(SpriteRenderer)))
+    {
+      item.material = _material;
+    }
+  }
+
+  void Update()
+  {
+  }
+}
