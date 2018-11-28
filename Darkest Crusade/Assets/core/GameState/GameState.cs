f3f4d5d444f13ac9b9f3993bc29b2fa3570be662
Aleksander Ujek <aleksander.ujek@gmail.com>
2018-11-28 22:13:10 +0100

    fix gamestate conflict

--- Darkest Crusade/Assets/core/GameState/GameState.cs
+++ Darkest Crusade/Assets/core/GameState/GameState.cs
@@ -7,15 +7,11 @@ namespace Core
 {
   public class GameState : Singleton<GameState>
   {
-<<<<<<< HEAD:Darkest Crusade/Assets/core/GameState.cs
     public ReactiveProperty<int> gold;
     public ReactiveCollection<GameObject> campHeroes;
     public ReactiveCollection<GameObject> churchHeroes;
     public ReactiveCollection<GameObject> tavernHeroes = new ReactiveCollection<GameObject>();
     public ReactiveCollection<GameObject> teamHeroes;
-=======
-    public List<GameObject> heroes;
     public DungeonState DungeonState;
->>>>>>> develop:Darkest Crusade/Assets/core/GameState/GameState.cs
   }
 }
