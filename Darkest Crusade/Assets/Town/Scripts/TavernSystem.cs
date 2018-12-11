c042f068e1e238247d5182484e50499ff5a02801
Aleksander Ujek <aleksander.ujek@gmail.com>
2018-12-11 11:11:39 +0100

    fixing prefab bugs with cloning values

--- Darkest Crusade/Assets/Town/Scripts/TavernSystem.cs
+++ Darkest Crusade/Assets/Town/Scripts/TavernSystem.cs
@@ -13,82 +13,41 @@ namespace Assets
 		
 		public GameObject heroListPanel;
 		public GameObject singleHeroPanel;
-		private HeroesFactory _heroFactory = new HeroesFactory();
+		private HeroesFactory _heroFactory;
 
 		// Use this for initialization
 		void Start () {
-			GameState.Instance.tavernHeroes = new ReactiveCollection<GameObject>(
-				new GameObject[] {
-				_heroFactory.SpawnOnUI(singleHeroPanel, heroListPanel, HeroStatusEnum.InTavern),
-				_heroFactory.SpawnOnUI(singleHeroPanel, heroListPanel, HeroStatusEnum.InTavern),
-				_heroFactory.SpawnOnUI(singleHeroPanel, heroListPanel, HeroStatusEnum.InTavern),
-				_heroFactory.SpawnOnUI(singleHeroPanel, heroListPanel, HeroStatusEnum.InTavern)
-			});
+			_heroFactory = new HeroesFactory();
 			PopulatePanel(GameState.Instance.tavernHeroes);
 		}
-		
-		// Update is called once per frame
-		void Update () {
-			
-		}
-
-		/// <summary>
-		/// This function is called when the object becomes enabled and active.
-		/// </summary>
 		void OnEnable()
 		{
 			PopulatePanel(GameState.Instance.tavernHeroes);
 		}
 
 		public void BuyHero(Button button, GameObject obj) {
-			
+			// #TODO
 		}
 
 		public void PopulatePanel(ReactiveCollection<GameObject> tavernHeroes) 
 		{
 			while (tavernHeroes.Count < 4)
 			{
-				GameState.Instance.tavernHeroes.Add(_heroFactory.SpawnOnUI(singleHeroPanel, heroListPanel, HeroStatusEnum.InTavern));
-			}
-			KillAllChildren(heroListPanel);
-			foreach (GameObject item in tavernHeroes)
-			{
-				GeneratePanel(item);
-
+				GameState.Instance.tavernHeroes.Add(GeneratePanel(singleHeroPanel));
 			}
 		}
-
-		public void GeneratePanel(GameObject heroObject) 
+		public GameObject GeneratePanel(GameObject heroObject) 
 		{
-			heroObject = _heroFactory.SpawnOnUI(singleHeroPanel, heroListPanel, HeroStatusEnum.InTavern);
-			TextMeshProUGUI[] heroClassTexts = heroObject.GetComponentsInChildren<TextMeshProUGUI>();
-			heroObject.GetComponentsInChildren<TextMeshProUGUI>()[0].text = heroObject.GetComponent<Hero>().HeroClass.ToString();
-			heroObject.GetComponentsInChildren<TextMeshProUGUI>()[1].text = heroObject.GetComponent<Hero>().Level.ToString();
-			heroObject.GetComponentsInChildren<TextMeshProUGUI>()[2].text = "Buy for\n" + heroObject.GetComponent<Hero>().HeroValue.Value.ToString() + " gold";
-			Image heroImage = heroObject.GetComponentInChildren<Image>();
+			GameObject newHero = _heroFactory.SpawnOnUI(heroObject, heroListPanel, HeroStatusEnum.InTavern);			
+			newHero.GetComponentsInChildren<TextMeshProUGUI>()[0].text = newHero.GetComponent<Hero>().HeroClass.ToString();
+			newHero.GetComponentsInChildren<TextMeshProUGUI>()[1].text = newHero.GetComponent<Hero>().Level.ToString();
+			newHero.GetComponentsInChildren<TextMeshProUGUI>()[2].text = "Buy for\n" + newHero.GetComponent<Hero>().HeroValue.Value.ToString() + " gold";
+			Image heroImage = newHero.GetComponentInChildren<Image>();
+			return newHero;
 		}
-
-		
-
-		private void KillAllChildren(GameObject parentObject) 
-		{
-			if (parentObject.transform.childCount!=0) {
-            	foreach (Transform child in parentObject.transform) {
-            	GameObject.Destroy(child.gameObject);
-            	}
-        	}
-		}
-
-
 		public bool ValidatePurchase(ReactiveProperty<int> value) 
 		{
 			return GameState.Instance.gold.Value > value.Value ? true : false;
 		}
-
-
-		
-
-
 	}
-	
 }
--- Darkest Crusade/Assets/Town/Scripts/TrinketShopSystem.cs
+++ Darkest Crusade/Assets/Town/Scripts/TrinketShopSystem.cs
@@ -14,34 +14,18 @@ public class TrinketShopSystem : MonoBehaviour {
     void Start()
     {
         _trinketFactory = new TrinketFactory();
-
         PopulatePanel(GameState.Instance.shopTrinkets);
+        
     }
-
     public void BuyTrinket(Button button, GameObject obj)
     {
         //TODO
     }
-
     public void PopulatePanel(ReactiveCollection<GameObject> shopTrinkets)
     {
         while (shopTrinkets.Count < 3)
         {
-            GameState.Instance.shopTrinkets.Add(_trinketFactory.SpawnOnUI(singleTrinketPanel, trinketListPanel));
-        }
-        KillAllChildren(trinketListPanel);
-        foreach (GameObject item in shopTrinkets)
-        {
-            GeneratePanel(item);
-        }
-    }
-
-    private void KillAllChildren(GameObject parentObject) 
-    {
-        if (parentObject.transform.childCount!=0) {
-            foreach (Transform child in parentObject.transform) {
-                GameObject.Destroy(child.gameObject);
-            }
+            GameState.Instance.shopTrinkets.Add(GeneratePanel(singleTrinketPanel));
         }
     }
     private string createString(Trinket trinket, bool isBuff)                                               // TODO Refactor
@@ -50,16 +34,15 @@ public class TrinketShopSystem : MonoBehaviour {
             ? trinket.Buff.BuffType + "\n" + "+" + trinket.Buff.BuffValue
             : (trinket.Debuff == null ? "" : trinket.Debuff.BuffType + "\n" + trinket.Debuff.BuffValue);
     }
-
-    public void GeneratePanel(GameObject trinketObject)
+    public GameObject GeneratePanel(GameObject trinketObject)
     {
-        trinketObject = _trinketFactory.SpawnOnUI(singleTrinketPanel, trinketListPanel);
-        trinketObject.GetComponentsInChildren<TextMeshProUGUI>()[0].text = trinketObject.GetComponent<Trinket>().TrinketClass.ToString();
-        trinketObject.GetComponentsInChildren<TextMeshProUGUI>()[1].text = createString(trinketObject.GetComponent<Trinket>(),true);
-        trinketObject.GetComponentsInChildren<TextMeshProUGUI>()[2].text = createString(trinketObject.GetComponent<Trinket>(), false);
-        trinketObject.GetComponentsInChildren<TextMeshProUGUI>()[3].text = "BUY\n" + trinketObject.GetComponent<Trinket>().TrinketValue.Value.ToString();
+        GameObject newTrinket = _trinketFactory.SpawnOnUI(trinketObject, trinketListPanel);
+        newTrinket.GetComponentsInChildren<TextMeshProUGUI>()[0].text = newTrinket.GetComponent<Trinket>().TrinketClass.ToString();
+        newTrinket.GetComponentsInChildren<TextMeshProUGUI>()[1].text = createString(newTrinket.GetComponent<Trinket>(),true);
+        newTrinket.GetComponentsInChildren<TextMeshProUGUI>()[2].text = createString(newTrinket.GetComponent<Trinket>(), false);
+        newTrinket.GetComponentsInChildren<TextMeshProUGUI>()[3].text = "BUY\n" + newTrinket.GetComponent<Trinket>().TrinketValue.Value.ToString();
+        return newTrinket;
     }
-
     public bool ValidatePurchase(ReactiveProperty<int> value)
     {
         return GameState.Instance.gold.Value > value.Value;
